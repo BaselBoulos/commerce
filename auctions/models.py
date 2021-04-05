e1dcf5e6c8b164e36d733efdb7d741624c8fa1bf
BaselBoulos <baselboulos@gmail.com>
2021-04-05 12:38:14 +1200

    Added New model for comments,

--- auctions/models.py
+++ auctions/models.py
@@ -14,8 +14,19 @@ class AuctionListing(models.Model):
     category = models.CharField(max_length=100)
     seller = models.CharField(max_length=100, default="Default_Value")
 
+    def __str__(self):
+        return f"{self.title} listing by {self.seller}"
 
 class WishList(models.Model):
     user = models.CharField(max_length=64)
     listing_id = models.IntegerField()
 
+
+class Comment(models.Model):
+    listing = models.ForeignKey(AuctionListing, on_delete=models.CASCADE, related_name="listing_comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment by {self.user} on {self.listing}"
